1c355ee03bd2c5449e6444904d58357d6fc128b4
Mathew E Newton <elijah2895@hotmail.com>
2021-02-05 00:44:20 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array, arrays)
+  array.concat(arrays)
+end
+
+def using_insert(array,index)
+  array.insert(4, index)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,integer)
+  array.delete_at(integer)
+end
